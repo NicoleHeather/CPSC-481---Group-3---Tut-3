24db5bfbcfc32c8c8c39cf1c4b26dff88d843918
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-24 14:01:10 -0600

    Better formatting and event conflict popup

--- assets/js/popupBookingRequest.js
+++ assets/js/popupBookingRequest.js
@@ -1,7 +1,18 @@
 // script.js
 const bookingConfirmtButton = document.getElementById('submit-info-btn');
 const bookingConfrimForm = document.getElementById('booking-request-popup');
+const time = document.querySelector('#time');
+const bookingConflictForm = document.getElementById('booking-request-conflict');
 
 bookingConfirmtButton.addEventListener('click', function () {
-    bookingConfrimForm.style.display = 'block';
+    value = time.value
+    console.log(value)
+
+    //If the time input is the same as the conflicting event time, show conflict popup.
+    if (value == "6PM") {
+        bookingConflictForm.style.display = 'flex';
+    }
+    else {
+        bookingConfrimForm.style.display = 'flex';
+    }
 });
