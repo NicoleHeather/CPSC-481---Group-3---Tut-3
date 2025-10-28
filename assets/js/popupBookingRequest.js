405ab846393b4f97e7c507b605732ede16ba484a
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-28 17:19:11 -0600

    Ensuring input looks the same as login page

--- assets/js/popupBookingRequest.js
+++ assets/js/popupBookingRequest.js
@@ -3,16 +3,19 @@ const bookingConfirmtButton = document.getElementById('submit-info-btn');
 const bookingConfrimForm = document.getElementById('booking-request-popup');
 const time = document.querySelector('#time');
 const bookingConflictForm = document.getElementById('booking-request-conflict');
+const background = document.getElementById('booking-event');
 
 bookingConfirmtButton.addEventListener('click', function () {
     value = time.value
     console.log(value)
 
     //If the time input is the same as the conflicting event time, show conflict popup.
-    if (value == "6PM") {
+    if (value == "6:00 PM" || value == "6 PM") {
         bookingConflictForm.style.display = 'flex';
+        background.style.filter = 'blur(5px)';
     }
     else {
         bookingConfrimForm.style.display = 'flex';
+        background.style.filter = 'blur(5px)';
     }
 });
