1e94a4d9c32ba753c8c9953e32bfc28b88f2588a
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-27 22:58:05 -0600

    Popup formatting fixes

--- assets/js/popupEventInfo.js
+++ assets/js/popupEventInfo.js
@@ -12,3 +12,22 @@ removeButton.addEventListener('click', function () {
 editButton.addEventListener('click', function () {
     editPopupForm.style.display = 'block';
 });
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
+const savedText = localStorage.getItem('textInput');
+const message = document.getElementById('message');
+
+if (savedText) {
+message.textContent = `Previously saved text: ${savedText}`;
+}
+
+const form = document.getElementById('textForm');
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const textInput = document.getElementById('textInput').value;
+localStorage.setItem('textInput', textInput);
+message.textContent = `Text saved: ${textInput}`;
+});
+});
+*/
