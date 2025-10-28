0db4c1a4f19bcc6fe87419da0422162949e7ae2f
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-28 17:54:48 -0600

    Currently adding saved info for editing and fixing formatting

--- assets/js/popupEventInfo.js
+++ assets/js/popupEventInfo.js
@@ -6,6 +6,13 @@ const removePopupForm = document.getElementById('removePopupForm');
 const background = document.getElementById('event-info');
 const shareButton = document.getElementById('event-share-btn');
 
+// Stored event info
+const eventDate = document.getElementById('current-date');
+const eventTime = document.getElementById('event-time');
+const eventLocation = document.getElementById('event-location');
+const eventCost = document.getElementById('event-cost');
+const eventDescription = document.getElementById('event-description');
+
 removeButton.addEventListener('click', function () {
     removePopupForm.style.display = 'block';
     background.style.filter = 'blur(5px)';
@@ -16,6 +23,8 @@ removeButton.addEventListener('click', function () {
  
 
 editButton.addEventListener('click', function () {
+    console.log(eventDate);
+
     editPopupForm.style.display = 'block';
     background.style.filter = 'blur(5px)';
     removeButton.style.filter = 'blur(5px)';
