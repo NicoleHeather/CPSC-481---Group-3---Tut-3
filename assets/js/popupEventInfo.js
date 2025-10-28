63b6d572a0f2ddbbecc5a738fca6e3c389277e4c
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-28 17:34:20 -0600

    Adding blur to background

--- assets/js/popupEventInfo.js
+++ assets/js/popupEventInfo.js
@@ -3,31 +3,22 @@ const editButton = document.getElementById('event-edit-btn');
 const editPopupForm = document.getElementById('editPopupForm');
 const removeButton = document.getElementById('event-remove-btn');
 const removePopupForm = document.getElementById('removePopupForm');
+const background = document.getElementById('event-info');
+const shareButton = document.getElementById('event-share-btn');
 
 removeButton.addEventListener('click', function () {
     removePopupForm.style.display = 'block';
+    background.style.filter = 'blur(5px)';
+    removeButton.style.filter = 'blur(5px)';
+    shareButton.style.filter = 'blur(5px)';
+    editButton.style.filter = 'blur(5px)';
 });
  
 
 editButton.addEventListener('click', function () {
     editPopupForm.style.display = 'block';
+    background.style.filter = 'blur(5px)';
+    removeButton.style.filter = 'blur(5px)';
+    shareButton.style.filter = 'blur(5px)';
+    editButton.style.filter = 'blur(5px)';
 });
-
-
-/*document.addEventListener('DOMContentLoaded', () => {
-const savedText = localStorage.getItem('textInput');
-const message = document.getElementById('message');
-
-if (savedText) {
-message.textContent = `Previously saved text: ${savedText}`;
-}
-
-const form = document.getElementById('textForm');
-form.addEventListener('submit', (event) => {
-event.preventDefault();
-const textInput = document.getElementById('textInput').value;
-localStorage.setItem('textInput', textInput);
-message.textContent = `Text saved: ${textInput}`;
-});
-});
-*/
