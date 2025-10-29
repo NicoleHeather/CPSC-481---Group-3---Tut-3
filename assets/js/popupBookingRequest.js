cf5bc3b785094c3de293fa4b54aef341e60b0a3d
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-28 20:42:53 -0600

    Adding grey out and covering buttons/input

--- assets/js/popupBookingRequest.js
+++ assets/js/popupBookingRequest.js
@@ -3,7 +3,7 @@ const bookingConfirmtButton = document.getElementById('submit-info-btn');
 const bookingConfrimForm = document.getElementById('booking-request-popup');
 const time = document.querySelector('#time');
 const bookingConflictForm = document.getElementById('booking-request-conflict');
-const background = document.getElementById('booking-event');
+const background = document.getElementById('whole-screen');
 
 bookingConfirmtButton.addEventListener('click', function () {
     value = time.value
@@ -12,10 +12,10 @@ bookingConfirmtButton.addEventListener('click', function () {
     //If the time input is the same as the conflicting event time, show conflict popup.
     if (value == "6:00 PM" || value == "6 PM") {
         bookingConflictForm.style.display = 'flex';
-        background.style.filter = 'blur(5px)';
+        background.style.background = 'lightgrey';
     }
     else {
         bookingConfrimForm.style.display = 'flex';
-        background.style.filter = 'blur(5px)';
+        background.style.background = 'lightgrey';
     }
 });
