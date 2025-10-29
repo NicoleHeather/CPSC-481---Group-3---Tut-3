792e501b7d20967ea1ef5e809a981d9f4819630e
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-28 20:19:09 -0600

    Adding save btn functionality

--- assets/js/popupBooking.js
+++ assets/js/popupBooking.js
@@ -1,7 +1,7 @@
 // script.js
-const bookingRequestButton = document.getElementById('booking-request-btn');
-const bookingPopupForm = document.getElementById('booking-request-form');
+const bookingRequestButton = document.getElementById('save-btn');
+const saveStatus = document.querySelector('save-btn');
 
 bookingRequestButton.addEventListener('click', function () {
-    bookingPopupForm.style.display = 'block';
+    
 });
