84cb815903f3eedc4b97de9cbb4ab644f7716c70
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-10-29 22:30:11 -0600

    Fixing grey out pop ups for event info and booking request
    Adding card background for event info as well.

--- assets/js/popupBookingRequest.js
+++ assets/js/popupBookingRequest.js
@@ -5,20 +5,21 @@ const time = document.querySelector('#time');
 const bookingConflictForm = document.getElementById('booking-request-conflict');
 const background = document.getElementById('whole-screen');
 const cardBackground = document.getElementById('booking-event')
+const modal = document.querySelector('.modal');
 
 bookingConfirmtButton.addEventListener('click', function () {
     
     value = time.value;
 
+    console.log(modal);
+
     //If the time input is the same as the conflicting event time, show conflict popup.
     if (value == "6:00 PM" || value == "6 PM") {
         bookingConflictForm.style.display = 'flex';
-        background.style.background = 'grey';
-        cardBackground.style.background = 'grey';
+        modal.style.display = "block";
     }
     else {
         bookingConfrimForm.style.display = 'flex';
-        background.style.background = 'grey';
-        cardBackground.style.background = 'grey';
+        modal.style.display = "block";
     }
 });
