b74cd0bd7fc6424f6b634852e263023da6e9803b
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-11-29 12:04:04 -0700

    Changes to event info and editing

--- assets/js/popupEventInfo.js
+++ assets/js/popupEventInfo.js
@@ -8,21 +8,24 @@ const shareButton = document.getElementById('event-share-btn');
 const modal = document.querySelector('.modal');
 const modal2 = document.querySelector('.modal-2');
 
-// Stored event info
+//Stored event info
 const currentTitle = document.querySelector('#event-title');
 const currentDate = document.querySelector('#current-date');
-const currentTime = document.querySelector('#current-time');
+const currentStart = document.querySelector('#current-start');
+const currentEnd = document.querySelector('#current-end');
 const currentLocation = document.querySelector('#current-location');
 const currentCost = document.querySelector('#current-cost')
 const currentDescription = document.querySelector('#current-description');
 
 //Input from form
-const titleInput = document.getElementById('title');
-const dateInput = document.getElementById('date');
-const timeInput = document.getElementById('time');
-const locationInput = document.getElementById('location');
-const costInput = document.getElementById('cost');
-const descriptionInput = document.getElementById('description');
+const titleInput = document.getElementById('ev-title');
+const dateInput = document.getElementById('ev-date');
+const startInput = document.getElementById('ev-start');
+const durationInput = document.getElementById('ev-dur');
+const endInput = document.getElementById('ev-end');
+const locationInput = document.getElementById('ev-location');
+const costInput = document.getElementById('ev-cost');
+const descriptionInput = document.getElementById('ev-notes');
 
 removeButton.addEventListener('click', function () {
     removePopupForm.style.display = 'block';
@@ -34,9 +37,23 @@ editButton.addEventListener('click', function () {
 
     titleInput.value = currentTitle.textContent;
     dateInput.value = currentDate.textContent;
-    timeInput.value = currentTime.textContent;
-    locationInput.value = currentLocation.textContent;
+    //startInput.innerText = currentStart.textContent;
+    endInput.value = currentEnd.textContent;
+
+    let help = document.createTextNode(currentStart.textContent)
+    startInput.innerHTML = '';
+    startInput.appendChild(help);
+
+    //Something is wrong with the spans where we are unable to select s start and duration
+    //As well as being unable to set span values on edit pop-up
     
+    let tmp1 = Number(startInput.textContent[0] + startInput.textContent[1]);
+    let tmp2 = Number(endInput.value[0] + endInput.value[1]);
+
+    console.log(String(tmp2 - tmp1));
+    durationInput.innerText = String(tmp2 - tmp1);
+
+    locationInput.value = currentLocation.textContent;
     costInput.value = currentCost.textContent;
     descriptionInput.value = currentDescription.textContent;
 
