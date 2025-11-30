8440c287b9e957ac64b5365756e2221127e19735
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-11-29 17:01:28 -0700

    Emergency info update

---
+++ assets/js/emergencyInfo.js
@@ -0,0 +1,12 @@
+// script.js
+const saveButton = document.getElementById('save-info-btn');
+
+//Stored event info
+const storedName = document.querySelector('#name-input');
+const storedPhone = document.querySelector('#phone-input');
+
+let saved = false;
+
+addEventListener.saveButton('click', function () {
+
+})
