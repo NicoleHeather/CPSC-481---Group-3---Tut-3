dc3b883448b80af9737113410d63cb1959d00c27
NicoleHeather <113859128+NicoleHeather@users.noreply.github.com>
2025-11-29 20:04:55 -0700

    Attempting refactoring of editing for event info

--- assets/js/popupEventInfo.js
+++ assets/js/popupEventInfo.js
@@ -8,8 +8,12 @@ const shareButton = document.getElementById('event-share-btn');
 const modal = document.querySelector('.modal');
 const modal2 = document.querySelector('.modal-2');
 
+const editForm = document.getElementById('event-form');
+const saveButton = document.getElementById('save-btn')
+console.log(editForm);
+
 //Stored event info
-const currentTitle = document.querySelector('#event-title');
+const currentTitle = document.querySelector('#event-title-stored');
 const currentDate = document.querySelector('#current-date');
 const currentStart = document.querySelector('#current-start');
 const currentEnd = document.querySelector('#current-end');
@@ -41,21 +45,23 @@ editButton.addEventListener('click', function () {
 
     titleInput.value = currentTitle.textContent;
     dateInput.value = currentDate.textContent;
-    //startInput.innerText = currentStart.textContent;
+    startInput.value = String(currentStart.textContent);
     endInput.value = currentEnd.textContent;
 
-    let help = document.createTextNode(currentStart.textContent)
+   // let help = document.createTextNode(currentStart.textContent)
     //startInput.innerHTML = '';
     //startInput.appendChild(help);
+    //console.log(startInput.selectedIndex);
+    durationInput.selectedIndex = "3";
 
-    //Something is wrong with the spans where we are unable to select a start and duration
+    //Something is wrong with <select> where we are unable to select a start and duration
     //As well as being unable to set span values on edit pop-up
     
-    let tmp1 = Number(startInput.textContent[0] + startInput.textContent[1]);
-    let tmp2 = Number(endInput.value[0] + endInput.value[1]);
+   // let tmp1 = Number(startInput.textContent[0] + startInput.textContent[1]);
+    //let tmp2 = Number(endInput.value[0] + endInput.value[1]);
 
     //console.log(String(tmp2 - tmp1));
-    //durationInput.innerText = String(tmp2 - tmp1);
+    //durationInput.value = String(tmp2 - tmp1);
 
     locationInput.value = currentLocation.textContent;
     costInput.value = currentCost.textContent;
@@ -64,12 +70,40 @@ editButton.addEventListener('click', function () {
     editPopupForm.style.display = 'block';
     modal.style.display = 'block';
 
-    if (customEvent){
-        console.log(dateInput);
-        console.log(document.getElementById('event-form'))
-        endInput.ariaReadOnly = true;
-        console.log(endInput)
-        //console.log(dateInput.ariaReadOnly = 'readOnly')
+    if (!customEvent){
+
+        dateInput.readOnly = true;
+        dateInput.style.color = 'grey';
+
+        startInput.readOnly = true;
+        startInput.style.color = 'grey';
+
+        //durationInput.readOnly = true;
+        durationInput.style.color = 'grey';
+        //disable not supported for select input types.
+
+        endInput.readOnly = true;
+        endInput.style.color = 'grey';
+
+        locationInput.readOnly = true;
+        locationInput.style.color = 'grey';
     }
-    
 });
+
+editForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    console.log("Here");
+    
+    currentTitle.textContent = titleInput.value;
+    //currentStart.textContent = currentStart.value;
+    currentEnd.textContent = endInput.value;
+    currentLocation.textContent = locationInput.value;
+    currentCost.textContent = costInput.value;
+    currentDescription.textContent = descriptionInput.value;
+
+    console.log(titleInput.value)
+    console.log(currentTitle.textContent)
+
+    //editForm.submit();
+})
